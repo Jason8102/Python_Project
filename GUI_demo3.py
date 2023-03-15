e192ea2c4a5984fcf40f97015ce1feee13bae1f1
Jason_ww8102 <panjunshen@sina.com>
2023-03-15 17:07:56 +0000

    3 pages

--- GUI_demo3.py
+++ GUI_demo3.py
@@ -24,32 +24,36 @@ root.minsize(height=480,width=800)
 #     button1.pack(side=BOTTOM)
 # tab1()
 
-def tab1(): #first page destroy
+def back01(): #first page destroy
     label1.destroy()
     button1.destroy()
     button2.destroy()
     
 
 def tab2():#second page
-    tab1()
+    back01()
     global label2
     label2 = Label(root,text="This is Second Tab",font=("Times_New_Roman",25))
     label2.pack()
     global button_1
-    button_1 = Button(root,text="BACK_2",font=("Times_New_Roman",25),activebackground="yellow",command=back)
+    button_1 = Button(root,text="BACK_2",font=("Times_New_Roman",25),activebackground="yellow",command=back02)
     button_1.pack(side=LEFT) 
     global button_2
-    button_2 = Button(root,text="NEXT_2",font=("Times_New_Roman",25),activebackground="green",command= tab1)
+    button_2 = Button(root,text="NEXT_2",font=("Times_New_Roman",25),activebackground="green",command= tab3)
     button_2.pack(side=RIGHT) 
     
 
-def back(): #second page destroy
+def back02(): #second page destroy
     label2.destroy()
     button_1.destroy()
     button_2.destroy()
-    tab0()
+    tab1()
+def back02_1(): #second page destroy
+    label2.destroy()
+    button_1.destroy()
+    button_2.destroy()
 
-def tab0(): # first page
+def tab1(): # first page
     global label1
     label1 = Label(root,text="This is First Tab",font=("Times_New_Roman",25))
     label1.pack(side=TOP)
@@ -60,16 +64,24 @@ def tab0(): # first page
     button2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command= tab2)
     button2.pack(side=RIGHT) 
 
-def tab3():
-    global label1
-    label1 = Label(root,text="This is First Tab",font=("Times_New_Roman",25))
-    label1.pack(side=TOP)
-    global button1
-    button1 = Button(root,text="BACK",font=("Times_New_Roman",25),activebackground="blue")
-    button1.pack(side=LEFT) 
-    global button2
-    button2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command= tab2)
-    button2.pack(side=RIGHT) 
+def tab3():# third page
+    back02_1()
+    global label3
+    label3 = Label(root,text="This is thrid Tab",font=("Times_New_Roman",25))
+    label3.pack(side=TOP)
+    global button3
+    button3 = Button(root,text="BACK_3",font=("Times_New_Roman",25),activebackground="blue")
+    button3.pack(side=LEFT) 
+    global button4
+    button4 = Button(root,text="NEXT_3",font=("Times_New_Roman",25),activebackground="red")
+    button4.pack(side=RIGHT) 
+
+def back03():
+    label3.destroy()
+    button3.destroy()
+    button4.destroy()
+
+#def tab4():
 
 #def tab2():
     # label2 = Label(root,text="This is Second Tab",font=("Times_New_Roman",25))
@@ -78,7 +90,7 @@ def tab3():
     # button2.pack(side=RIGHT) 
 
 
-tab0()
+tab1()
 #tab2()
 
 root.mainloop()
