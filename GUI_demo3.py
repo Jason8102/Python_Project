7a777b26393fdbf1ca5acef2fc2bd206220858f7
Jason_ww8102 <panjunshen@sina.com>
2023-03-15 15:33:57 +0000

    add some labels

--- GUI_demo3.py
+++ GUI_demo3.py
@@ -25,25 +25,17 @@ root.minsize(height=480,width=800)
 #     button1.pack(side=BOTTOM)
 # tab1()
 def tab1(): 
-    label1 = Label(root,text="This is First Tab",font=("Times_New_Roman",25))
-    label1.pack(side=TOP)
-    button1 = Button(root,text="BACK",font=("Times_New_Roman",25),activebackground="blue")
-    button1.pack(side=LEFT) 
-    button2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command= tab2)
-    button2.pack(side=RIGHT)
+    label1.destroy()
+    button1.destroy()
+    button2.destroy()
     tab2()
-    def back2():
-        label1.destroy()
-        button1.destroy()
-        button2.destroy()
-    
 
 def tab2():
     label2 = Label(root,text="This is Second Tab",font=("Times_New_Roman",25))
     label2.pack()
     button_1 = Button(root,text="BACK",font=("Times_New_Roman",25),activebackground="blue", commond =back)
     button_1.pack(side=LEFT) 
-    button_2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command=)
+    button_2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command= tab1)
     button_2.pack(side=RIGHT) 
     
 
@@ -53,7 +45,12 @@ def tab2():
         button_2.destroy()
         tab1()
 
- 
+label1 = Label(root,text="This is First Tab",font=("Times_New_Roman",25))
+label1.pack(side=TOP)
+button1 = Button(root,text="BACK",font=("Times_New_Roman",25),activebackground="blue")
+button1.pack(side=LEFT) 
+button2 = Button(root,text="NEXT",font=("Times_New_Roman",25),activebackground="red",command= tab2)
+button2.pack(side=RIGHT) 
 
 
 #def tab2():
@@ -63,7 +60,7 @@ def tab2():
     # button2.pack(side=RIGHT) 
 
 
-tab1()
+#tab1()
 #tab2()
 
 root.mainloop()
